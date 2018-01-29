ff5edbab174d969dec988fe3147c2b2897ce48f8
Yining <yiningcherry@gmail.com>
2018-01-29 22:02:02 +1100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './app.routing';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { JobsComponent } from './jobs/jobs.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    FooterComponent,
+    JobsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
