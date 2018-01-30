458a4eb1c29816b24045fe4604c8d97861523500
Yining <yiningcherry@gmail.com>
2018-01-31 08:45:27 +1100

    pipes

---
+++ src/app/currency-converter.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currencyConverter'
+})
+export class CurrencyConverterPipe implements PipeTransform {
+
+  transform(value: number, args?: any): any {
+    return value/100;
+  }
+
+}
